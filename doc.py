ec719f28ac289843654aa4f47bfb9e36d12ccd83
xzdil <82775283+xzdil@users.noreply.github.com>
2024-04-05 16:06:41 +0500

    samfksd

--- doc.py
+++ doc.py
@@ -1,13 +1,12 @@
 import requests
 
-def download_QA():
-    url = "https://www.dropbox.com/scl/fi/vnbhfi2eu1ciuwjwzsnnn/QA.docx?rlkey=3dpojwnrpt4f34q9gx461yode&dl=1"
-    file_path = "docs/QA.docx"
+url = "https://www.dropbox.com/scl/fi/vnbhfi2eu1ciuwjwzsnnn/QA.docx?rlkey=3dpojwnrpt4f34q9gx461yode&dl=1"
+file_path = "docs/QA.docx"
 
-    response = requests.get(url)
-    response.raise_for_status()
+response = requests.get(url)
+response.raise_for_status()
 
-    with open(file_path, 'wb') as f:
-        f.write(response.content)
+with open(file_path, 'wb') as f:
+    f.write(response.content)
 
-    print(f"Файл успешно скачан и сохранен как {file_path}")
+print(f"Файл успешно скачан и сохранен как {file_path}")
