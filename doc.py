6f884257cb9ec499a21a81416edbb7882a705ed3
xzdil <82775283+xzdil@users.noreply.github.com>
2024-04-05 16:04:31 +0500

    Create doc.py

---
+++ doc.py
@@ -0,0 +1,13 @@
+import requests
+
+def download_QA():
+    url = "https://www.dropbox.com/scl/fi/vnbhfi2eu1ciuwjwzsnnn/QA.docx?rlkey=3dpojwnrpt4f34q9gx461yode&dl=1"
+    file_path = "docs/QA.docx"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(file_path, 'wb') as f:
+        f.write(response.content)
+
+    print(f"Файл успешно скачан и сохранен как {file_path}")
