f2763d6e58676a715ed4ddf460d8fbb3eb7bcdaf
xzdil <82775283+xzdil@users.noreply.github.com>
2024-04-05 16:57:25 +0500

    Update faiss2.py

--- faiss2.py
+++ faiss2.py
@@ -6,10 +6,15 @@ from llama_index.core import (
 )
 from llama_index.vector_stores.faiss import FaissVectorStore
 from model import llm
+import faiss
 
 documents = SimpleDirectoryReader("docs").load_data()
 
-vector_store = FaissVectorStore()
+# dimensions of text-ada-embedding-002
+d = 1536
+faiss_index = faiss.IndexFlatL2(d)
+
+vector_store = FaissVectorStore(faiss_index=faiss_index)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 index = VectorStoreIndex.from_documents(
     documents, storage_context=storage_context
